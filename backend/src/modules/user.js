50e54f93d215724dcc48f9c02f6f2830cbf2fce0
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-05-30 02:40:17 +0300

    add the encryptjs to make the password encrypted

--- backend/src/modules/user.js
+++ backend/src/modules/user.js
@@ -1,61 +1,77 @@
-import mongoose, { Schema } from 'mongoose'; 
-// the time stamps will help us to know the user from when he signup and these things 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        require: true,
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+// the time stamps will help us to know the user from when he signup and these things
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
     },
-    lastName:{
-        type: String,
-        require: true,
+    lastName: {
+      type: String,
+      require: true,
     },
-    email:{
-        type: String ,
-        require: true ,
-        unique: true ,
+    email: {
+      type: String,
+      require: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        require: true ,
-        minlength:7,
+    password: {
+      type: String,
+      require: true,
+      minlength: 7,
     },
-    bio:{
-        type:String , 
-        default:"",
+    bio: {
+      type: String,
+      default: "",
     },
-    profilePic:{
-        type:String,
-        default:"",
+    profilePic: {
+      type: String,
+      default: "",
     },
-    nativeLanguage:{
-        type:String,
-        default:"",
+    nativeLanguage: {
+      type: String,
+      default: "",
     },
-    learningLanguage:{
-        type:String,
-        default:"",
+    learningLanguage: {
+      type: String,
+      default: "",
     },
-    location:{
-        type:String,
-        default:""
+    location: {
+      type: String,
+      default: "",
     },
     // this what will decide if the user is able to go to other pages or stay at the signup mission
-    inOnBoarding:{
-        type:Boolean,
-        default:false,
+    inOnBoarding: {
+      type: Boolean,
+      default: false,
     },
-    // so when you friend with someone you will take his id and put it in your friends array of objects that will contain the other users id 
-    friends:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"User",
-        },
-    ]
-},
-{timestamps:true});
+    // so when you friend with someone you will take his id and put it in your friends array of objects that will contain the other users id
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-// then we will have to create a module based on our schema 
 
-const User = mongoose.model("User",userSchema);
+// then we will have to create a module based on our schema
+const User = mongoose.model("User", userSchema);
 
-export default User; 
+// now to make a password hashing with the pre hook with salt and salt importance is to make the main password with some encryption
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next(); 
+  try {
+    const salt = await bcrypt.getSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default User;
